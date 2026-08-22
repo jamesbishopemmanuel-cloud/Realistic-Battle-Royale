4950b7c6317d4e40f7902bae5080bfef3fef50e0
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-22 21:35:35 +0100

    Create Assets/Scripts/UI/MainMenuManager.cs

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+
+
+    public void StartGame()
+    {
+        Debug.Log(
+        "Loading Battle Royale...");
+    }
+
+
+    public void OpenInventory()
+    {
+        Debug.Log(
+        "Opening inventory");
+    }
+
+
+    public void OpenShop()
+    {
+        Debug.Log(
+        "Opening shop");
+    }
+
+}
