a61b7a7c16ffdf7bded4740ec5792fb68aa29038
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-23 00:14:38 +0100

    Update inventory.gd

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -6,18 +6,28 @@ var items: Array[String] = []
 
 
 func add_item(item: String) -> bool:
+
     if items.size() >= max_slots:
         print("Inventory full")
         return false
 
     items.append(item)
-    print("Added: ", item)
+
+    print("Picked up: ", item)
+
     return true
 
 
 func remove_item(item: String):
+
     items.erase(item)
 
 
 func has_item(item: String) -> bool:
+
     return items.has(item)
+
+
+func get_items():
+
+    return items
