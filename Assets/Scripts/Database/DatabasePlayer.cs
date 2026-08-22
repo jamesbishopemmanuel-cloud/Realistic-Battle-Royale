f584dfd1f9271479202550f252d4e042b43d78a6
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-22 22:10:38 +0100

    Create Assets/Scripts/Database/DatabasePlayer.cs

---
+++ Assets/Scripts/Database/DatabasePlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public class DatabasePlayer
+{
+
+    public string id;
+
+    public string username;
+
+    public int level;
+
+    public int coins;
+
+    public int wins;
+
+}
