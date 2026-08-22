e97e16e9473b4535d43ff8d44071efd016d77070
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-22 22:30:16 +0100

    Create Assets/Scripts/Database/DatabaseConnector.cs

---
+++ Assets/Scripts/Database/DatabaseConnector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+public class DatabaseConnector : MonoBehaviour
+{
+
+    public void SavePlayer()
+    {
+        Debug.Log(
+        "Saving player database");
+    }
+
+
+    public void LoadPlayer()
+    {
+        Debug.Log(
+        "Loading player database");
+    }
+
+}
