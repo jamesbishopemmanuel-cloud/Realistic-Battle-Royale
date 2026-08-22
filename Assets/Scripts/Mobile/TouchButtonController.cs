a9655efe3457bb167104f39eed788076927f906e
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-22 21:51:08 +0100

    Create Assets/Scripts/Mobile/TouchButtonController.cs

---
+++ Assets/Scripts/Mobile/TouchButtonController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+public class TouchButtonController : MonoBehaviour
+{
+
+    public void FireButton()
+    {
+        Debug.Log("Fire");
+    }
+
+
+    public void JumpButton()
+    {
+        Debug.Log("Jump");
+    }
+
+
+    public void ReloadButton()
+    {
+        Debug.Log("Reload");
+    }
+
+}
