ab2abafd79b613a9860fce12bfecb667f94f75d1
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-22 21:43:20 +0100

    Create Assets/Scripts/AI/AdvancedEnemyAI.cs

---
+++ Assets/Scripts/AI/AdvancedEnemyAI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AdvancedEnemyAI : MonoBehaviour
+{
+
+    public Transform target;
+
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent =
+        GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if(target != null)
+        {
+            agent.SetDestination(
+            target.position);
+        }
+    }
+
+}
