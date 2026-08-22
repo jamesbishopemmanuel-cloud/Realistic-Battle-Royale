461abc328eebe8b97589fa7da78051c01c9869b4
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-22 22:29:28 +0100

    Update AuthenticationSystem.cs

--- Assets/Scripts/Backend/AuthenticationSystem.cs
+++ Assets/Scripts/Backend/AuthenticationSystem.cs
@@ -1,1 +1,33 @@
-Assets/Scripts/Backend/AuthenticationSystem.cs
+using UnityEngine;
+
+
+public class AuthenticationSystem : MonoBehaviour
+{
+
+    public string playerToken;
+
+
+    public void Login(
+    string username,
+    string password)
+    {
+
+        playerToken =
+        "TOKEN_" + username;
+
+
+        Debug.Log(
+        "Login successful");
+
+    }
+
+
+    public void Logout()
+    {
+        playerToken = "";
+
+        Debug.Log(
+        "Logged out");
+    }
+
+}
