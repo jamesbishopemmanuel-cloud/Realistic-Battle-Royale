8f58e458b5411e7842ed6cfb76fd81c504cef44d
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-23 01:25:02 +0100

    Create Source/RealisticBattleRoyale/RealisticBattleRoyale.cpp

---
+++ Source/RealisticBattleRoyale/RealisticBattleRoyale.cpp
@@ -0,0 +1,8 @@
+#include "RealisticBattleRoyale.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(
+    FDefaultGameModuleImpl,
+    RealisticBattleRoyale,
+    "RealisticBattleRoyale"
+);
